248ed43fa742579c7e2f6da37ddc7d84ddd0b6ac
Bishruti Siku <bishrutisiku7@gmail.com>
2016-01-01 19:00:44 +0100

    Change path

--- src/adapter/server/ApplicationConfig.java
+++ src/adapter/server/ApplicationConfig.java
@@ -3,13 +3,16 @@ package adapter.server;
 import adapter.ExceptionListener;
 import org.glassfish.jersey.server.ResourceConfig;
 
+import javax.ws.rs.ApplicationPath;
+
 /**
  * Created by bishruti on 12/23/15.
  */
 
+@ApplicationPath("adapter")
 public class ApplicationConfig extends ResourceConfig {
     public ApplicationConfig () {
-        packages("ehealth");
+        packages("adapter");
         register(ExceptionListener.class);
     }
 }
